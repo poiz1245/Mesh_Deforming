2c02b948fc2c4091ac97a9cffe38373af7bbf079
poiz1245 <wva960@gmail.com>
2024-05-20 10:24:41 +0900

    프로젝트 업로드

---
+++ Mesh_Deforming/Assets/Scripts/MeshDeformer.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using Unity.VisualScripting.FullSerializer;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class MsshDeformer : MonoBehaviour
+{
+    Mesh deformingMesh;
+    Vector3[] originalVertices, displacedVertices;
+    Vector3[] vertexVelocities;
+
+    public float force = 10f;
+
+    void Start()
+    {
+        deformingMesh = GetComponent<MeshFilter>().mesh;
+        originalVertices = deformingMesh.vertices;
+        displacedVertices = new Vector3[originalVertices.Length];
+        for (int i = 0; i < originalVertices.Length; i++)
+        {
+            displacedVertices[i] = originalVertices[i];
+        }
+
+        vertexVelocities = new Vector3[originalVertices.Length];
+    }
+}
